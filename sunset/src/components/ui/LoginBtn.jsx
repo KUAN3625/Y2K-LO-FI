64fe58edf6a47f57dcba3103141d29bf7346735e
Kuan👾 <kuan3625@gmail.com>
2025-10-14 21:47:46 +0800

    feat: 重構LoginBtn元件的點擊邏輯，將導航處理移至handleClick函數以增強可讀性

--- sunset/src/components/ui/LoginBtn.jsx
+++ sunset/src/components/ui/LoginBtn.jsx
@@ -1,22 +1,29 @@
 import { useNavigate } from "react-router-dom";
 
-   
+
 
 const Loginbtn = (props) => {
+  const navigate = useNavigate();
 
-const navigate = useNavigate();
+  const handleClick = () => {
+    if (props.onBeforeNavigate) {
+      props.onBeforeNavigate()
+    }
+    navigate(props.to)
+
+  }
 
   return (
-      <div onClick={()=> navigate(props.to)}
-       className={`pointer-events-auto px-8 h-10 border-2 border-black rounded-full w-64 grid place-items-center mx-auto
+    <div onClick={handleClick}
+      className={`pointer-events-auto px-8 h-10 border-2 border-black rounded-full w-64 grid place-items-center mx-auto
      hover:shadow-2xl hover:bg-black hover:text-white
      ${props.className || ""}`}
-      >
+    >
 
-        {props.nametext}
+      {props.nametext}
 
 
-      </div>
+    </div>
 
   );
 };
