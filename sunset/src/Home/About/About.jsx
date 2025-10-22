98ee2aea54a9d39b3826a6482bc2b64f52f332cc
Kuan👾 <kuan3625@gmail.com>
2025-10-22 15:37:25 +0800

    完成

--- sunset/src/Home/About/About.jsx
+++ sunset/src/Home/About/About.jsx
@@ -1,7 +1,10 @@
+import SideMenu from "../../components/ui/Settings_UI/Side"
+
 const About =()=> {
 
     return(
-        <div>
+        <div className=" flex">
+        <SideMenu/>
         <h1>關於頁面
         </h1>
         <p>請看</p>
--- sunset/src/Home/Core/Core.jsx
+++ sunset/src/Home/Core/Core.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import Musicbox from "../../components/tool-page/music/Musicplay"
 import TimerPanel from "../../components/tool-page/Pomodoro/TimePanel"
 import { Eye, EyeOff } from "lucide-react";
+import SideMenu from "../../components/ui/Settings_UI/Side";
 
 
 
@@ -9,13 +10,15 @@ const Core =() =>{
     const [isHidden, setIsHidden] = useState(false);
 
     return(
-        <main className="relative w-full min-h-screen text-gray-800 px-6 py-8">
+        <main className="pointer-events-auto relative w-full min-h-screen text-gray-800 px-6 py-8">
         
         <div
         className={`transition-opacity duration-500 ${
           isHidden ? "opacity-0 pointer-events-none" : "opacity-100"
         }`}
       >
+        <SideMenu />
+
         <TimerPanel />
 
         <Musicbox />
