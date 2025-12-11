a7de7f61f7528b4c02198d31ebfbf0df89b52897
Kuan👾 <kuan3625@gmail.com>
2025-12-11 20:41:04 +0800

    新增多項音樂

--- src/data/song.js
+++ src/data/song.js
@@ -8,20 +8,38 @@ export const songsdata = [
   {
     title: "Waiting Around",
     artist: "Holizna",
-    coverUrl: "/COVERS/75.webp",
+    coverUrl: "/COVERS/74.webp",
     url: "/MUSIC/HoliznaCC0 - Waiting Around.mp3",
   },
   {
     title: "One Night In France",
     artist: "Holizna",
-    coverUrl: "/COVERS/76.webp",
+    coverUrl: "/COVERS/74.webp",
     url: "/MUSIC/HoliznaCC0 - One Night In France.mp3",
   },
   {
     title: "When Time Called Me Darling",
     artist: "Holizna",
-    coverUrl: "/COVERS/77.webp",
+    coverUrl: "/COVERS/74.webp",
     url: "/MUSIC/HoliznaCC0 - When Time Called Me Darling.mp3",
+  },
+  {
+    title: "Canon Event",
+    artist: "Holizna",
+    coverUrl: "/COVERS/74.webp",
+    url: "/MUSIC/HoliznaCC0 - Canon Event.mp3",
+  },
+  {
+    title: "Bubbles",
+    artist: "Holizna",
+    coverUrl: "/COVERS/74.webp",
+    url: "/MUSIC/HoliznaCC0 - Bubbles.mp3",
+  },
+  {
+    title: "Yet Again",
+    artist: "Holizna",
+    coverUrl: "/COVERS/74.webp",
+    url: "/MUSIC/HoliznaCC0 - Yet Again.mp3",
   },
 
 
